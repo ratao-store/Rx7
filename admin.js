0c1639285c431aed16d90b1091cb06481ac3d300
ratao-store <rataoconta17@gmail.com>
2026-06-19 22:47:51 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,41 @@
 
+function enter(){
+
+if(
+code.value
+!==
+"admin1234"
+){
+
+alert(
+"Acesso negado"
+)
+
+return
+
+}
+
+let pedido=
+
+JSON.parse(
+
+localStorage
+.getItem(
+"pedido"
+)
+
+)
+
+panel.innerHTML=
+
+`
+ID:
+${pedido?.id}
+
+<br><br>
+
+Status:
+${pedido?.status}
+`
+
+}
