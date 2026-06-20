f4e8e6a6e8ddf086395254455da4641e6fff2915
ratao-store <rataoconta17@gmail.com>
2026-06-19 22:48:11 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,73 @@
+function openLogin(){
 
+login.style.display=
+"flex"
+
+}
+
+function register(){
+
+localStorage
+.setItem(
+
+"user",
+
+user.value
+
+)
+
+localStorage
+.setItem(
+
+"pass",
+
+pass.value
+
+)
+
+alert(
+"Conta criada"
+)
+
+}
+
+function login(){
+
+if(
+
+user.value===
+
+localStorage
+.getItem(
+"user"
+)
+
+&&
+
+pass.value===
+
+localStorage
+.getItem(
+"pass"
+)
+
+){
+
+alert(
+"Entrou"
+)
+
+login.style.display=
+"none"
+
+}
+
+else{
+
+alert(
+"Dados inválidos"
+)
+
+}
+
+}
